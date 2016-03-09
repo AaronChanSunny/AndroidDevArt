cce2c1255e433678d8bc0109cf6793a7aab80d8f
Aaron Chan <aaronchansunny@gmail.com>
2016-03-09 23:04:22 +0800

    multi process activities

--- app/src/main/java/com/aaron/androiddevart/MainActivity.java
+++ app/src/main/java/com/aaron/androiddevart/MainActivity.java
@@ -1,15 +1,18 @@
 package com.aaron.androiddevart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                UserManager.sUserId = 2;
+
+                Log.d(TAG, "user id " + UserManager.sUserId);
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/aaron/androiddevart/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.aaron.androiddevart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Log.d(TAG, "user id " + UserManager.sUserId);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, ThirdActivity.class));
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
